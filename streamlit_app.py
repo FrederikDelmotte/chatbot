36a43f47942f2ebc62f0cf5d38149f83a0166ed4
FrederikDelmotte <reinout.delmotte@gmail.com>
2026-08-19 16:02:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 PZ VA Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Via deze chatbot kan u ondersteuning vinden voor het nieuwe strafwetboek. "
+    "Stel uw vraag zonder het vermelden van persoonsgegevens. "
+    "Succes!"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
